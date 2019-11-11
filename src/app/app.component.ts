d6e528e295cf048b7490e1cf8252e5bee8394476
Leonardo Souza <lsouzafapos@gmail.com>
2019-11-11 16:29:47 -0200

    Exercicio 5 e 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { templateJitUrl } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
@@ -7,8 +8,54 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   nomes: string[] = ['joao', 'maria', 'jose', 'pedro', 'felipe', 'carlos'];
+  nomesFiltro: string[];
+  pessoas: any = [
+    {id: 1, nome: 'joao', salario: 5000},
+    {id: 1, nome: 'maria', salario: 1000},
+    {id: 1, nome: 'jose', salario: 2000},
+    {id: 1, nome: 'pedro', salario: 3000},
+    {id: 1, nome: 'felipe', salario: 10000},
+    {id: 1, nome: 'carlos', salario: 800},
+  ]
+
+  getValorTotal(): Number{
+    return this.pessoas.reduce(
+      (soma, pessoa) => soma + pessoa.salario,0);
+  }
 
   buscar(valor: string){
-    alert(valor)
+    this.nomesFiltro = [];
+
+    //metodo 1
+    for(var i = 0; i< this.nomes.length; i++){
+      if(this.nomes[i].toLowerCase().includes(valor.toLowerCase())){
+        this.nomesFiltro.push(this.nomes[i]);
+      }
+    }
+  
+
+  //metodo 2
+  let temp = [];
+  this.nomes.forEach(buscarItem);
+  function buscarItem(nome){
+    if (nome.toLowerCase().includes(valor.toLowerCase())){
+      
+      temp.push(nome);
+    }
   }
+  
+  this.nomesFiltro = temp;
+
+  //metodo 3
+  this.nomesFiltro = this.nomes.filter(function (nome) {
+    return nome.toLowerCase().includes(valor.toLowerCase());
+  });
+  
+  //metodo 4
+  this.nomesFiltro = this.nomes.filter(
+    (nome) => nome.toLowerCase().includes(valor.toLowerCase()));
+  
+  }
+
 }
+
