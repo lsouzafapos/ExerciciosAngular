b503505d287ea85040546ee519de7090304b5133
Leonardo Souza <lsouzafapos@gmail.com>
2019-11-11 16:38:22 -0200

    Exercicio 7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,16 +11,20 @@ export class AppComponent {
   nomesFiltro: string[];
   pessoas: any = [
     {id: 1, nome: 'joao', salario: 5000},
-    {id: 1, nome: 'maria', salario: 1000},
-    {id: 1, nome: 'jose', salario: 2000},
-    {id: 1, nome: 'pedro', salario: 3000},
-    {id: 1, nome: 'felipe', salario: 10000},
-    {id: 1, nome: 'carlos', salario: 800},
+    {id: 2, nome: 'maria', salario: 1000},
+    {id: 3, nome: 'jose', salario: 2000},
+    {id: 4, nome: 'pedro', salario: 3000},
+    {id: 5, nome: 'felipe', salario: 10000},
+    {id: 6, nome: 'carlos', salario: 800},
   ]
 
   getValorTotal(): Number{
     return this.pessoas.reduce(
       (soma, pessoa) => soma + pessoa.salario,0);
+  }
+
+  buscarId(id){
+    return this.pessoas.find(pessoa => pessoa.id == id);
   }
 
   buscar(valor: string){
