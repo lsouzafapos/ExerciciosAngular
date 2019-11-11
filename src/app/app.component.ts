0aa3cd734f137b0c8ec58b1e715f8965fb1d5a17
Leonardo Souza <lsouzafapos@gmail.com>
2019-11-11 15:46:39 -0200

    Exemplo 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ExerciciosAngular';
+  nomes: string[] = ['joao', 'maria', 'jose', 'pedro', 'felipe', 'carlos'];
 }
